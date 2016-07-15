2e4c87006e9067114200b839b97c3eec588c841a
sagar <sgrpwr@live.com>
2016-07-15 16:39:44 +0530

    Directory Listing from server

--- app/src/main/java/com/fantasticfive/shareback/newshareback/Constants.java
+++ app/src/main/java/com/fantasticfive/shareback/newshareback/Constants.java
@@ -5,7 +5,7 @@ package com.fantasticfive.shareback.newshareback;
  */
 public class Constants {
 
-    public static final String IP_FILE_SERVER = "172.16.160.84";
+    public static final String IP_FILE_SERVER = "172.16.160.106";
 
     public static final int PORT_FILE_RECV = 8110;
     public static final int PORT_LS = 8103;
